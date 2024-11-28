cbe2f147ae61de1c7ae44fe1caa8f59571746f1a
Mike Lynch <mj.lynch23@gmail.com>
2024-11-27 21:39:53 -0700

    Base Models and Fine Tunes table updated

--- app/controllers/fine_tunes_controller.rb
+++ app/controllers/fine_tunes_controller.rb
@@ -1,32 +1,71 @@
 class FineTunesController < ApplicationController
+  before_action :set_fine_tune, only: [:edit, :update, :destroy]
   def index
-    @fine_tunes = FineTune.includes(:base_model).where("fine_tunes.name LIKE ?", "%#{params[:filter]}%")
+    @fine_tunes = FineTune.includes(:base_model).order(created_at: :desc)
+    @fine_tunes = @fine_tunes.where("name LIKE ?", "%#{params[:filter]}%")
   end
 
   def new
-    @fine_tune = fine_tune.new
+    @fine_tune = FineTune.new
   end
 
   def create
-    @fine_tune = fine_tune.new(fine_tune_params)
+    @fine_tune = FineTune.new(fine_tune_params)
 
     if @fine_tune.save
-      # Need a flash message here
-      render turbo_stream: turbo_stream.append(
-        dom_id(@fine_tunes),
-        partial: "shared/resource_table",
-        locals: { item: @fine_tune,
-        headers: {
-          "Fine Tune" => 'name',
-          "Base Model" => "base_model.name"
-          }
-        }
-      )
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.append("fine_tunes", partial: "fine_tune", locals: { fine_tune: @fine_tune }),
+          turbo_stream.replace("new-fine-tune-form", partial: "form", locals: { fine_tune: FineTune.new })
+        ]
+      end
+      format.html { redirect_to fine_tunes_path, notice: "Fine tune was successfully created." }
+    end
     else
-      render turbo_stream: turbo_stream.replace(
-        dom_id(@fine_tune, "form"),
-        locals: { fine_tune: @fine_tune }
-      )
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("new-fine-tune-form", partial: "form", locals: { fine_tune: @fine_tune })
+        end
+        format.html { render :index, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    respond_to do |format|
+      format.turbo_stream do
+        render partial: "edit", locals: { fine_tune: @fine_tune }
+      end
+      format.html
+    end
+  end
+
+  def update
+    if @fine_tune.update(fine_tune_params)
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(dom_id(@fine_tune), partial: "fine_tune", locals: { fine_tune: @fine_tune })
+        end
+        format.html { redirect_to fine_tunes_path, notice: "Fine tune was successfully updated." }
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(dom_id(@fine_tune), partial: "edit", locals: { fine_tune: @fine_tune })
+        end
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @fine_tune.destroy
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.remove(dom_id(@fine_tune))
+      end
+      format.html { redirect_to fine_tunes_path, notice: "Fine tune was successfully deleted." }
     end
   end
 
@@ -34,5 +73,9 @@ class FineTunesController < ApplicationController
 
   def fine_tune_params
     params.require(:fine_tune).permit(:name, :base_model_id)
+  end
+
+  def set_fine_tune
+    @fine_tune = FineTune.find(params[:id])
   end
 end
